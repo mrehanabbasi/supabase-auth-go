b08877a85b365bea92abf82d45d9d27cd7e9168b
M. Rehan Abbasi <mrehanabbasi@yahoo.com>
2025-02-20 08:33:00 +0500

    add custom functions for initializing client

--- client.go
+++ client.go
@@ -15,6 +15,11 @@ type client struct {
 	*endpoints.Client
 }
 
+type Config struct {
+	BaseURL string
+	APIKey  string
+}
+
 // Set up a new Auth client.
 //
 // projectReference: The project reference is the unique identifier for your
@@ -29,6 +34,39 @@ type client struct {
 func New(projectReference string, apiKey string) Client {
 	return &client{
 		Client: endpoints.New(projectReference, apiKey),
+	}
+}
+
+// Set up a new Auth client with custom auth URL.
+//
+// cfg: cfg is the configuration struct which contains custom auth URL
+// and API Key. The API key is used to authenticate requests to the Auth server.
+// This should be your anon key.
+//
+// This function does not validate your auth URL. Requests will fail
+// if you pass in an invalid auth URL.
+func NewWithCustomAuthURL(cfg Config) Client {
+	return &client{
+		Client: endpoints.New("", cfg.APIKey).WithCustomAuthURL(cfg.BaseURL),
+	}
+}
+
+// Set up a new Auth client with custom auth URL.
+//
+// cfg: cfg is the configuration struct which contains custom auth URL
+// and API Key. The API key is used to authenticate requests to the Auth server.
+// This should be your anon key.
+//
+// c: This is the stdlib *http.Client if you want to use your own HTTP client.
+//
+// This function does not validate your auth URL. Requests will fail
+// if you pass in an invalid auth URL.
+func NewWithCustomAuthURLAndHTTPClient(cfg Config, c *http.Client) Client {
+	return &client{
+		Client: endpoints.
+			New("", cfg.APIKey).
+			WithCustomAuthURL(cfg.BaseURL).
+			WithClient(c),
 	}
 }
 
